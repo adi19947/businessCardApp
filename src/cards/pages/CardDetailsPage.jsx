0538c893a81b5ac97c5fd0a88ac3bf888a09a3a3
adi19947 <dano93@gmail.com>
2023-05-01 18:12:30 +0300

    commit

--- src/cards/pages/CardDetailsPage.jsx
+++ src/cards/pages/CardDetailsPage.jsx
@@ -1,28 +1,40 @@
-import { CardMedia, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import { useParams } from "react-router-dom";
 import PageHeader from "../../components/PageHeader";
 import { useCardsContext } from "../../providers/CardsProvider";
-import CardHead from "../components/card/CardHead";
 
 export default function CardDetailsPage() {
   const { id } = useParams();
   const { value } = useCardsContext();
-  const { cards } = value;
+  const { filteredCards } = value;
 
-  const myCard = cards?.find((card) => {
+  const myCard = filteredCards?.find((card) => {
     return card._id == id;
   });
-
+  console.log(myCard);
   return (
     <>
       <PageHeader
         title="Card details"
         subtitle="Here you can find all the details about specific card"
       />
-      <Container>
-        <CardHead image={myCard.image} />
+
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <img
+            src={`/${myCard.image.url}`}
+            alt="business card image"
+            style={{ maxWidth: "100%", height: "auto", borderRadius: "20%" }}
+          />
+        </Box>
       </Container>
     </>
   );
